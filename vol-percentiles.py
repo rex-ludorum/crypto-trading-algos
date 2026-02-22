dec5b97efb20cfbe6fba714e3182358758718f03
Tony Lu <konigderspiele@live.com>
2026-02-21 18:03:33 -0600

    Modify percentiles script to output volumes for testing

--- vol-percentiles.py
+++ vol-percentiles.py
@@ -1,80 +1,64 @@
-import datetime
-import dateutil
-import numpy as np
-
-def getMicrosecondsFromDate(trade):
-	date = trade[1] + "T" + trade[2] + "Z"
-	parsedDate = dateutil.parser.isoparse(date)
-	return round(datetime.datetime.timestamp(parsedDate) * 1000000)
-
-trades = []
-with open('BTC-USD_2024-07-18-23.00.00.000000000_2024-07-22-02.00.00.000000000', 'r') as f:
-	for line in f:
-		data = line.split(" ")
-		data[-1] = data[-1][:-1]
-		trades.append(data)
+import argparse
 
 fifteenMinInMicroseconds = 15 * 60 * 1000000
-windows = [fifteenMinInMicroseconds * i for i in list(range(1, 17))]
-timeWindows = [[0, getMicrosecondsFromDate(trades[0])] for _ in windows]
+windows = [fifteenMinInMicroseconds * i for i in list(range(1, 5))]
+timeWindows = [[0, 0] for _ in windows]
 
 buyVols = [0] * len(windows)
 sellVols = [0] * len(windows)
 
-allBuyVols = [[] for _ in windows]
-allSellVols = [[] for _ in windows]
-startCollectingVols = [False] * len(windows)
+trades = []
 
-# 60th percentile to 95th percentile in increments of 5 percentiles
-buyVolPercentiles = [[] for _ in windows]
-sellVolPercentiles = [[] for _ in windows]
-
-for idx, trade in enumerate(trades):
-	vol = float(trade[4])
-	microseconds = getMicrosecondsFromDate(trade)
-
-	for j, window in enumerate(windows):
-		if microseconds - timeWindows[j][1] > window:
-			startCollectingVols[j] = True
-			i = timeWindows[j][0]
-			while i < idx:
-				newMicroseconds = getMicrosecondsFromDate(trades[i])
-				if microseconds - newMicroseconds > windows[j]:
-					newVol = float(trades[i][4])
-					if trades[i][5] == 'false':
-						buyVols[j] -= newVol
+def calculateIndicators():
+	tradeIdx = 0
+	first = True
+	for inputFile in inputFiles:
+		with open(inputFile, "r") as file:
+			file.readline()
+			for line in file:
+				data = line.split(",")
+				trades.append(data)
+				vol = float(data[5]) * float(data[4])
+				timestamp = int(data[0])
+				if first:
+					for window in timeWindows:
+						window[1] = timestamp
+					first = False
+				isBuyerMaker = data[6].strip().lower() == 'true'
+				tradeIdx += 1
+				for j, window in enumerate(windows):
+					if timestamp - timeWindows[j][1] > window:
+						i = timeWindows[j][0]
+						while i < tradeIdx - 1:
+							newMicroseconds = int(trades[i][0])
+							if timestamp - newMicroseconds > windows[j]:
+								newVol = float(trades[i][5]) * float(trades[i][4])
+								if trades[i][6].strip().lower() == 'true':
+									sellVols[j] -= newVol
+								else:
+									buyVols[j] -= newVol
+							else:
+								break
+							i += 1
+						timeWindows[j] = [i, int(trades[i][0])]
+					if isBuyerMaker:
+						sellVols[j] += vol
 					else:
-						sellVols[j] -= newVol
-				else:
-					break
-				i += 1
-			timeWindows[j] = [i, getMicrosecondsFromDate(trades[i])]
-		if trade[5] == 'false':
-			buyVols[j] += vol
-		else:
-			sellVols[j] += vol
-		if startCollectingVols[j]:
-			allBuyVols[j].append(buyVols[j])
-			allSellVols[j].append(sellVols[j])
+						buyVols[j] += vol
+				if tradeIdx % 100000 == 0:
+					print("Timestamp: %d" % timestamp)
+					print("15 minute buy volume: %f" % buyVols[0])
+					print("15 minute sell volume: %f" % sellVols[0])
+					print("30 minute buy volume: %f" % buyVols[1])
+					print("30 minute sell volume: %f" % sellVols[1])
+					print("45 minute buy volume: %f" % buyVols[2])
+					print("45 minute sell volume: %f" % sellVols[2])
+					print("60 minute buy volume: %f" % buyVols[3])
+					print("60 minute sell volume: %f" % sellVols[3])
 
-for idx, arr in enumerate(allBuyVols):
-	npArr = np.array(arr)
-	for percentile in range(60, 100, 5):
-		buyVolPercentiles[idx].append(np.percentile(npArr, percentile))
-for idx, arr in enumerate(allSellVols):
-	npArr = np.array(arr)
-	for percentile in range(60, 100, 5):
-		sellVolPercentiles[idx].append(np.percentile(npArr, percentile))
+parser = argparse.ArgumentParser(description='Show cumulative volume calculations for sanity checking.')
+parser.add_argument('files', nargs="+", help='the files across which to calculate')
+args = parser.parse_args()
+inputFiles = vars(args)['files']
 
-buyVolPercentiles = [[x.item() for x in sublist] for sublist in buyVolPercentiles]
-sellVolPercentiles = [[x.item() for x in sublist] for sublist in sellVolPercentiles]
-
-with open("buyPercentiles", "w") as f:
-	for sublist in buyVolPercentiles:
-		strlist = [str(x) for x in sublist]
-		f.write(" ".join(strlist) + '\n')
-
-with open("sellPercentiles", "w") as f:
-	for sublist in sellVolPercentiles:
-		strlist = [str(x) for x in sublist]
-		f.write(" ".join(strlist) + '\n')
+calculateIndicators()
